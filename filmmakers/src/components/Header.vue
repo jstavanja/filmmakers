17716b21e38b9239e475f74b23e31512287aa50b
jstavanja <jstavanja@gmail.com>
2017-07-13 18:33:19 +0200

    add: temporary header component design

---
+++ filmmakers/src/components/Header.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="navbar">
+        <div class="logo"></div>
+        <div class="navbar-links">
+            <div class="centering-wrapper">
+                <div class="navbar-item" v-for="navLink in navLinks">
+                    <a href="#">{{ navLink.name }}</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                navLinks: [{name: "Hot"}, {name: "Marketplace"}, {name: "Editor's picks"}, {name: "Stories"}, {name: "People"}]
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    // fontface import
+    @import url('https://fonts.googleapis.com/css?family=Rasa');
+
+    .navbar {
+
+        background-image: linear-gradient(60deg, #29323c 0%, #485563 100%);
+
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 50px;
+        padding: 10px 0 10px 0;
+
+        border-bottom: 1px solid black;
+
+        .navbar-links {
+
+            display: inline-block;
+            height: 100%;
+
+            .centering-wrapper {
+                height: 100%;
+                display: table;
+
+                .navbar-item {
+
+                    display: table-cell;
+                    vertical-align: middle;
+                    padding-left: 30px;
+
+                    a {
+                        font-family: 'Helvetica', serif;
+                        color: white;
+                        text-decoration: none;
+                        font-size: 16px;
+                        font-weight: 200;
+                        display: inline-block;
+                        
+                        &:hover {
+                            text-decoration: underline;
+                            text-decoration-color: white;
+                        }
+                    }
+                }
+            }
+        }
+
+        .logo {
+            background-image: url('../assets/logo.png');
+            background-repeat: no-repeat;
+            background-size: contain;
+            display: inline-block;
+            width: 300px;
+            margin-left: 30px;
+            margin-right: 30px;
+            height: 100%;
+        }
+
+    }
+</style>
--- filmmakers/src/main.js
+++ filmmakers/src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Header from './components/Header.vue';
+import VueRouter from 'vue-router';
+
+// Components
+Vue.component('app-header', Header);
+
+// Router
+Vue.use(VueRouter);
 
 new Vue({
   el: '#app',
