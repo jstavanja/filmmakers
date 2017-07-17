cdaa0201e8e20fd08deb517586d53899ce63d20e
jstavanja <jstavanja@gmail.com>
2017-07-17 21:35:56 +0200

    add: add simple structure for stories

--- filmmakers/src/components/Stories/Stories.vue
+++ filmmakers/src/components/Stories/Stories.vue
@@ -1,5 +1,31 @@
 <template>
-    <div>
-        The stories component
+    <div class="page-content">
+        <div class="col-xs-12 col-sm-12 col-md-6 col-md-6" v-for="(story, index) in stories">
+            <app-story :story="story" :key="index"></app-story>
+        </div>
     </div>
 </template>
+
+<script>
+    import Story from './Story.vue';
+    export default {
+        data() {
+            return {
+                stories: []
+            }
+        },
+        methods: {
+          fetchStories() {
+              this.$http.get('stories.json')
+                  .then(data => data.json())
+                  .then(result => this.stories = result);
+          }
+        },
+        components: {
+           appStory: Story
+        },
+        created() {
+            this.fetchStories();
+        }
+    }
+</script>
---
+++ filmmakers/src/components/Stories/Story.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            {{ story.title }}
+        </div>
+        <div class="panel-body">
+            <p>{{ story.description }}</p>
+            <button class="btn btn-success">Read more</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [
+            'story'
+        ]
+    }
+</script>
+
+<style lang="scss" scoped>
+    .panel {
+        .panel-body {
+            p {
+                display: inline-block;
+                width: 80%;
+            }
+            .btn {
+                display: inline-block;
+                float: right;
+            }
+        }
+    }
+</style>
