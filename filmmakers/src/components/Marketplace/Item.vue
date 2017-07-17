2130c17571a6faf494141d37610d18bc2d07f70e
jstavanja <jstavanja@gmail.com>
2017-07-17 17:35:46 +0200

    add: marketplace items basic setup and idea

---
+++ filmmakers/src/components/Marketplace/Item.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <div class="align-left">
+                    {{ item.name }}
+                </div>
+                <div class="align-right">
+                    {{ item.price }} $
+                </div>
+            </div>
+            <!-- Smaller screens -->
+            <div class="panel-body hidden-sm hidden-md hidden-lg">
+                <div class="row">
+                    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                        <!-- TODO: implement img src="{{ item.image }}" -->
+                        <img src="../../assets/avatar.jpg" alt="" class="item-image">
+                    </div>
+                    <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+                        {{ item.description }}
+                    </div>
+                </div>
+                <div class="row text-right see-more">
+                    <button class="btn btn-success">See more!</button>
+                </div>
+            </div>
+            <!-- Larger screens -->
+            <div class="panel-body hidden-xs">
+                <div class="row image">
+                    <!-- TODO: implement img src="{{ item.image }}" -->
+                    <img src="../../assets/avatar.jpg" alt="" class="item-image">
+                </div>
+                <div class="row description">
+                    {{ item.description }}
+                </div>
+                <div class="row text-center see-more">
+                    <button class="btn btn-success">See more!</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [
+            'item'
+        ]
+    }
+</script>
+
+<style lang="scss">
+    .panel {
+        .panel-heading {
+            div {
+                position: relative;
+                display: inline-block;
+            }
+            .align-right {
+                float: right;
+                color: forestgreen;
+            }
+        }
+        .panel-body {
+            margin: 10px;
+            padding-top: 0;
+            .description {
+                margin-top: 10px;
+            }
+            .item-image {
+                border-radius:5px;
+                width: 100%;
+            }
+            .see-more {
+                margin-top: 10px;
+            }
+        }
+    }
+
+    @media (max-width: 550px) {
+        .hidden-sm {
+            display: none;
+        }
+        .hidden-xs {
+            display: block !important;
+        }
+    }
+</style>
--- filmmakers/src/components/Marketplace/Marketplace.vue
+++ filmmakers/src/components/Marketplace/Marketplace.vue
@@ -1,5 +1,25 @@
 <template>
-    <div>
-        The marketplace component
+    <div class="page-content">
+        <app-item v-for="(item, index) in items" :item="item" :key="index"></app-item>
     </div>
 </template>
+
+<script>
+    import Item from './Item.vue';
+
+    export default {
+        data() {
+            return {
+                items: [
+                    {name: "Vintage preset", image: "", description: "Quick and easy 50's vibe preset for Premiere.", price: "10"},
+                    {name: "Intro template", image: "", description: "Dubsteppy, glitchy, etc. Basically an intro template for After Effects.", price: "20"},
+                    {name: "Cutting course", image: "", description: "Quick 2h course where you learn how to cut videos to make a fluid composition.", price: "15"},
+                    {name: "Cinematic music", image: "", description: "Hans Zimmer inspired theme music for your video's background.", price: "35"}
+                ]
+            }
+        },
+        components: {
+            appItem: Item
+        }
+    }
+</script>
