3913b4c50207a0d132331e45bbe97cad5aaa400b
jstavanja <jstavanja@gmail.com>
2017-07-17 22:08:19 +0200

    add: add simple structure for people component

--- filmmakers/src/components/People/People.vue
+++ filmmakers/src/components/People/People.vue
@@ -1,5 +1,46 @@
 <template>
-    <div>
-        The people component
+    <div class="page-content">
+        <div class="row">
+            <div class="panel panel-default jumbotron text-center">
+                <h1>Featured creators</h1>
+                <p>Discover new people that are creating amazing things.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6" v-for="(person, index) in people">
+                <app-person :person="person"></app-person>
+            </div>
+        </div>
     </div>
 </template>
+
+<script>
+    import Person from './Person.vue';
+
+    export default {
+        data() {
+            return {
+                people: []
+            }
+        },
+        methods: {
+            fetchPeople() {
+                this.$http.get('people.json')
+                    .then(data => data.json())
+                    .then(result => this.people = result);
+            }
+        },
+        components: {
+          appPerson: Person
+        },
+        created() {
+            this.fetchPeople();
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .panel {
+        padding-left: 30px;
+    }
+</style>
---
+++ filmmakers/src/components/People/Person.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <span class="name">{{ person.name }}</span>
+            <button class="btn btn-sm btn-success">Read about {{ person.name.split(" ")[0] }}</button>
+        </div>
+        <div class="panel-body">
+            <div class="row"><img src="../../assets/avatar.jpg" alt=""></div>
+            <div class="row text-center">
+                {{ person.bio }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['person']
+    }
+</script>
+
+<style lang="scss" scoped>
+    .panel {
+        .panel-heading {
+            padding-bottom: 10px;
+            .name {
+                font-size: 20px;
+            }
+            .btn {
+                float: right;
+                height: 100%;
+            }
+        }
+        .panel-body {
+            * {
+                padding: 10px;
+            }
+            img {
+                width: 100%;
+            }
+        }
+    }
+</style>
