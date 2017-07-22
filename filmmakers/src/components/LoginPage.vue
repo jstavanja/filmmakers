f3635fee12c15aba6ab3588dd965ac3f57d41a3f
jstavanja <jstavanja@gmail.com>
2017-07-22 11:23:57 +0200

    fix: already logged in text v-if addition

--- filmmakers/src/components/LoginPage.vue
+++ filmmakers/src/components/LoginPage.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="page-content">
         <app-login-form class="login-form" isComingFrom="loginPage" v-if="!loggedIn"></app-login-form>
-        <div class="jumbotron">
+        <div class="jumbotron" v-if="loggedIn">
             <h1>You are already logged in :)</h1>
             <router-link to="/" tag="button" class="btn btn-success">Go back home</router-link>
         </div>
@@ -19,6 +19,9 @@
                 return this.$store.getters.loggedIn;
             }
         },
+        created() {
+            document.querySelector('.sidebar-wrapper').style.display = 'none'; // re-show the sidebar
+        },
         beforeDestroy() {
             document.querySelector('.sidebar-wrapper').style.display = 'block'; // re-show the sidebar
         }
@@ -26,9 +29,6 @@
 </script>
 
 <style lang="scss">
-    .sidebar-wrapper {
-        display: none;
-    }
     .login-form {
         width: 75%;
     }
